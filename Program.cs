c4690e2df96c9310e8c8f5105c17a0388b76b444
ipiyumii <sendtopiyumi@gmail.com>
2024-11-26 20:11:41 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number A: ");
+        int numberA = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter number B: ");
+        int numberB = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("What is the value of" + numberA + "and" + numberB + ": ");
+        int answerInput = Convert.ToInt32(Console.ReadLine());
+
+        int actulaAnswer = numberA * numberB;
+
+        while(answerInput != actulaAnswer) 
+        {
+            Console.WriteLine("Answer is wrong");
+
+            Console.WriteLine("Enter your answer again:  ");
+            answerInput = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Correct answer!");
+    }
+}
